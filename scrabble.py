438fad8584758791e5a0f5c1c4d89fd5841365ef
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-05 16:45:37 +0000

    Scrabble program first iteration

--- scrabble.py
+++ scrabble.py
@@ -9,9 +9,24 @@ class Scrabble(Score):
 
     def scrabble_score(self):
         total_score = 0
-
+    # Iterates over each letter in the word
         for i in self.word:
+            # Turns the word into lowercase
             i = i.lower()
+            #adds the score based on each letter
             total_score = total_score + self.score[i]
         return total_score
 
+def main():
+    # Asks the player to input a word
+    word = input("Please provide a word\n=> ")
+
+    # Assigning an object to the class to begin the process
+    game = Scrabble(word.lower())
+
+    # Prints the total score for the word
+    print(f"Word Scored: {game.scrabble_score()} points")
+
+
+if __name__ == "__main__":
+    main()
